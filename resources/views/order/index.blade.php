8d6e9379fe6763b8cada777e6ecb5dfd1897567f
Andres Felipe Penna <felipe.penna@correounivalle.edu.co>
2022-03-29 22:44:09 -0500

    feat: Add table example to index view from Order

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -1,4 +1,27 @@
 @extends('layout.layout')
 @section('content')
-    <h1> el contenido {{ $message}}</h1>
+    <h1> Order List</h1>
+    <table class="table">
+        <thead class="table-dark">
+            <tr>
+                <th>Customer name</th>
+                <th>Email</th>
+                <th>Mobile</th>
+                <th>Status order</th>
+                <th>Product name</th>
+                <th>Price</th>
+
+            </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Andres Felipe</td>
+            <td>andres.felipe.penna@gmail.com</td>
+            <td>3118452133</td>
+            <td>CREATED</td>
+            <td>Computer</td>
+            <td>4000000</td>
+        </tr>
+        </tbody>
+    </table>
 @endsection
