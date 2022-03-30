80da58ea58db3914e092ddf42e3505ca0abee76a
Andres Felipe Penna <felipe.penna@correounivalle.edu.co>
2022-03-29 22:43:17 -0500

    feat: add boostrap support to layout template

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -4,12 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Demo shop</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
     <!-- Styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
         html, body {
             background-color: #fff;
@@ -64,9 +67,9 @@
     </style>
 </head>
 <body>
-<div class="flex-center position-ref full-height">            <div class="content">
+<div class="flex-center position-ref full-height">
+    <div class="content">
         @yield("content")
-
     </div>
 </div>
 </body>
