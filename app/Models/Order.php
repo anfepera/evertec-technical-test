4deb3b7f2ad3da6ee3463937908210a2b9f8f6dd
Andres Felipe Penna <felipe.penna@correounivalle.edu.co>
2022-03-29 21:33:45 -0500

    feat: add filleable attrib to Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    /**
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        "customer_name",
+        "customer_email",
+        "customer_mobile",
+        "status"
+    ];
 }
