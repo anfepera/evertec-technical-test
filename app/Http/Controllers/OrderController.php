fd046046bd5564fe3daf9014461c2fea8678f221
Andres Felipe Penna <felipe.penna@correounivalle.edu.co>
2022-03-30 21:17:30 -0500

    feat: persist order register

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,20 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function  index() {
+    public function index() {
         $orders = DB::table('orders')->get();
         return view('order.index', [ "orders" => $orders]);
     }
 
-    public function  new() {
+    public function new() {
         return view('order.new');
     }
-    public function  pay() {
-        return ('pay method!');
+    public function pay(Request $request) {
+        $data = $request->all();
+        ;
+        $order = new Order([
+            "customer_name" => $data['customer_name'],
+            "customer_email" => $data['email'],
+            "customer_mobile" => $data['phone_number'],
+            "status" => "CREATED"]
+        );
+        $order->save();
+        return $this->index();
     }
 
 }
