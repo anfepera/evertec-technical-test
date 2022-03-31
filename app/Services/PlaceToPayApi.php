b53cb307e0fa8186d16d32b36bcf8b46d1b7a77d
Andres Felipe Penna <felipe.penna@correounivalle.edu.co>
2022-03-31 15:14:49 -0500

    feat: add customer name, email and mobile to placeToPay request

--- app/Services/PlaceToPayApi.php
+++ app/Services/PlaceToPayApi.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\API;
+namespace App\Services;
 use Dnetix\Redirection\PlacetoPay;
 use Illuminate\Support\Facades\Request;
 
@@ -11,41 +11,55 @@ class PlaceToPayApi
          * Create instance of placeToPay service
          * [https://github.com/dnetix/redirection]
          */
-        $this->placetopay = new PlacetoPay([
+
+    }
+
+    /**
+     * @param $data
+     * @return void
+     */
+    public function  createPaymentRequest($data){
+        /**
+         * Provide information for make to pay and return url of payment
+         */
+        $data_response = [];
+        $placetopay = new PlacetoPay([
             'login' => '6dd490faf9cb87a9862245da41170ff2', // Provided by PlacetoPay
             'tranKey' => '024h1IlD', // Provided by PlacetoPay
             'baseUrl' => 'https://dev.placetopay.com/redirection/',
             'timeout' => 10, // (optional) 15 by default
         ]);
-    }
-    public function  create_payment_request($reference, $amount) {
-        /**
-         * Provide information for make to pay and return url of payment
-         */
         $request = [
+            'buyer' => [
+                'name' => $data['customer_name'],
+                'email' => $data['customer_email'],
+                'mobile' => $data['customer_mobile']
+            ],
             'payment' => [
-                'reference' => $reference,
+                'reference' => $data['reference'],
                 'description' => 'Testing payment',
                 'amount' => [
                     'currency' => 'COP',
-                    'total' => $amount,
+                    'total' => $data['product_price'],
                 ],
             ],
             'expiration' => date('c', strtotime('+2 days')),
-            //'returnUrl' => 'http://example.com/response?reference=' . $reference,
+            'returnUrl' => 'http://evertec-technical-test.loc:8000/order/detail/' . $data['reference'],
             'ipAddress' => Request::ip(),
-            'userAgent' => Request::userAgent(),
+            'userAgent' => Request::userAgent()
         ];
-
-        $response = $this->placetopay->request($request);
+        $response = $placetopay->request($request);
         if ($response->isSuccessful()) {
             // STORE THE $response->requestId() and $response->processUrl() on your DB associated with the payment order
             // Redirect the client to the processUrl or display it on the JS extension
-            //$response->processUrl();
+            $data_response['url'] = $response->processUrl();
+            $data_response['id_transaction'] = $response->requestId();
+
         } else {
             // There was some error so check the message and log it
-            //$response->status()->message();
+            $response->status()->message();
         }
+        return $data_response;
 
     }
 
