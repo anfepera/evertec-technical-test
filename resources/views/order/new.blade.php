ae30abe00a26f79589dd71be541faa68411d617d
Andres Felipe Penna <felipe.penna@correounivalle.edu.co>
2022-03-30 20:44:07 -0500

    feat: add form field en create order view

--- resources/views/order/new.blade.php
+++ resources/views/order/new.blade.php
@@ -1,6 +1,54 @@
 @extends('layout.layout')
 @section('content')
     <h1> Create Order</h1>
-    <a href="{{ route('order.pay')}}" class="btn btn-warning">Pay</a>
+    <form method="POST" action="{{ route('order.pay') }}">
+        @csrf
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col">
+                    <label for="customer_name">Customer name:</label>
+                    <input id="customer_name"
+                           name="customer_name"
+                           type="text"
+                           class="@error('customer_name') is-invalid @enderror">
 
+                </div>
+            </div>
+            <div class="row justify-content-md-center">
+                <div class="col">
+                    <label for="email">Email:</label>
+                    <input id="email"
+                           name="email"
+                           type="email"
+                           class="@error('email') is-invalid @else is-valid @enderror">
+
+                </div>
+            </div>
+            <div class="row justify-content-md-center">
+                <div class="col">
+                    <label for="phone_number">Phone number:</label>
+                    <input id="phone_number"
+                           name="phone_number"
+                           type="number"
+                           class="@error('phone_number') is-invalid @enderror">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <button type="submit" class="btn btn-success"  value="Submit">Pay</button>
+                </div>
+            </div>
+        </div>
+    </form>
+
+
+    @error('customer_name')
+    <div class="alert alert-danger"> Customer name is required</div>
+    @enderror
+    @error('email')
+    <div class="alert alert-danger"> Email is required</div>
+    @enderror
+    @error('phone_number')
+    <div class="alert alert-danger"> Phone number is required</div>
+    @enderror
 @endsection
