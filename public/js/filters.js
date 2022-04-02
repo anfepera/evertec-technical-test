8b523ea85d6801cda5973cd5eda59a975b01a228
Andres Felipe Penna <felipe.penna@correounivalle.edu.co>
2022-04-02 17:59:57 -0500

    feat: redirect  route "/" to product index

--- public/js/filters.js
+++ public/js/filters.js
@@ -4,8 +4,16 @@ var __webpack_exports__ = {};
   !*** ./resources/js/filters.js ***!
   \*********************************/
 $(document).ready(function () {
+  $("#email_row").hide();
+  $("#product_row").show();
   $('#type_filter').change(function () {
-    alert($(this).val());
+    if ($(this).val() == "id_product") {
+      $("#email_row").hide();
+      $("#product_row").show();
+    } else {
+      $("#email_row").show();
+      $("#product_row").hide();
+    }
   });
 });
 /******/ })()
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,5 +1,54 @@
 @extends('layout.layout')
 @section('content')
+    <div class="card" >
+        <div class="card-body">
+            <h5 class="card-title">Orders Filters</h5>
+            <p class="card-text">Get order list by customer email or product.</p>
+
+                <form method="POST" action="{{ route('order.pay') }}">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <label for="type_filter">Filter By:</label>
+                            </div>
+                            <div class="col">
+                                <select name="type_filter" id="type_filter">
+                                    <option value="id_product">Product</option>
+                                    <option value="customer_email">Customer Email</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row" id="product_row">
+                            <div class="col">
+                                <label for="list_products">Products:</label>
+                            </div>
+                            <div class="col">
+                                <select name="list_products" id="list_products">
+                                    @foreach ($products as $product)
+                                        <option value="{{ $product->id }}">{{  $product->product_name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div id="email_row" class="row">
+                            <div class="col">
+                                <label for="filter_email">Customer Email:</label>
+                            </div>
+                            <div class="col">
+                                <input id="filter_email"
+                                       name="filter_email"
+                                       type="email"
+                                       placeholder="Please, input the email">
+
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+
+            <a href="#" class="btn btn-primary">Get orders</a>
+        </div>
+    </div>
     <h1> Product List</h1>
     <table class="table">
         <thead class="table-dark">
